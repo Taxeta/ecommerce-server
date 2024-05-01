cdc620e70708dea0c0db1f8136f2005caef07eed
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2024-05-01 23:42:04 +0200

    Create users tables with supabase method database

--- src/server/supabase/supabase.js
+++ src/server/supabase/supabase.js
@@ -2,6 +2,4 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseKey);
